b28fac5ec41774e0bcffc83308c25e378d817a09
istyanadira <istyanadiraa@gmail.com>
2025-09-22 19:48:12 +0700

    SESI 1

---
+++ tugas1.js
@@ -0,0 +1,13 @@
+let matematika = 85;
+let BahasaInggris = 90;
+let ipa = 78;
+
+let NilaiRataRata = (matematika + BahasaInggris + ipa) / 3;
+
+console.log("Nilai rata-rata:", NilaiRataRata);
+
+if (NilaiRataRata >= 80) {
+    console.log("Status: Lulus");
+} else {
+    console.log("Status: Tidak Lulus");
+}
